f75bd2cf1c370745a2402a7b3a94c33d9db0c7e9
Henadzi Arashkevich <oreshkevich85@mail.ru>
2022-06-29 13:17:08 +0300

    redux

---
+++ src/store/positions/position-selectors.js
@@ -0,0 +1,14 @@
+export const selectAllPositions = (state) => state.positions;
+
+export const selectVisiblePositions = (state, filters = []) => {
+  if (filters.length === 0) return state.positions;
+  return state.positions.filter((pos) => {
+    const postFilters = [].concat(
+      pos.role,
+      pos.level,
+      ...pos.languages,
+      ...pos.tools
+    );
+    return filters.every((filter) => postFilters.includes(filter));
+  });
+};
